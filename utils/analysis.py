24ca110ad517f085b4cca1ed9743ebc94ab93464
Juan Garcia Giraldo <juan.garciagiraldo@student.manchester.ac.uk>
2023-11-11 16:02:17 +0100

    Cleaned analysis on article connections

--- utils/analysis.py
+++ utils/analysis.py
@@ -1,1 +1,26 @@
 """ Module to store all functions related to data analysis tasks """
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def visualize_article_connections_per_category(connections, description):
+  graph = nx.DiGraph()
+  for _, row in connections.iterrows():
+    start_category = row['start_broad_category']
+    end_category = row['end_broad_category']
+    if graph.has_edge(start_category, end_category):
+      graph[start_category][end_category]['weight'] += 1
+    else:
+      graph.add_edge(start_category, end_category, weight=1)
+
+  edge_weights = [graph[u][v]['weight'] for u, v in graph.edges()]
+  max_edge_weight = max(edge_weights)
+  min_edge_weight = min(edge_weights)
+  normalized_edge_weights = [(weight - min_edge_weight) / (max_edge_weight - min_edge_weight) for weight in edge_weights]
+  edge_widths = [weight * 4 for weight in normalized_edge_weights]
+
+  figure = nx.shell_layout(graph)
+  plt.figure(figsize=(8, 8))
+  nx.draw(graph, figure, with_labels=True, width=edge_widths, edge_color='gray', arrows=True)
+  plt.title(description)
+  plt.show()
--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,1 +1,47 @@
 """ Module to store all functions related to data preprocessing """
+
+import pandas as pd
+
+# functions to get all links between articles
+from itertools import tee
+
+def pairwise(iterable):
+  # from python docs - will be introduced in version 3.10
+  # pairwise('ABCDEFG') --> AB BC CD DE EF FG
+  a, b = tee(iterable)
+  next(b, None)
+  return zip(a, b)
+
+def get_all_links(df, path_colname="path"):
+  edge_counter = {}
+  for _, row in df.iterrows():
+    
+    links = row['path']
+    edges = list(pairwise(links))
+
+    for edge in edges:
+      if edge in edge_counter:
+        edge_counter[edge] += 1
+      else:
+        edge_counter[edge] = 1
+
+  out = pd.Series(edge_counter).reset_index()
+  out.columns = ["source", "target", "weight"]
+  return out
+
+def merge_articles_categories(df, left_on, articles_categories):
+  merged_df = df.copy()
+
+  merged_df = merged_df.merge(articles_categories, how="left", left_on=left_on[0], right_on="article").rename(columns = {
+    "category": "start_category",
+    "broad_category": "start_broad_category",
+  })
+
+  merged_df = merged_df.merge(articles_categories, how="left", left_on=left_on[1], right_on="article").rename(columns = {
+    "category": "end_category",
+    "broad_category": "end_broad_category",
+  })
+
+  merged_df = merged_df.drop(['article_x', 'article_y'], axis=1)
+  #display(merged_df.head())
+  return merged_df

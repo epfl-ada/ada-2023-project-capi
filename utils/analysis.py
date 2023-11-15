4a3a4fa0dcaef0946e1eca90f9b4ac4b28c015c7
arcivelekoglu <acivelekoglu@gmail.com>
2023-11-15 01:58:36 +0100

    todos done according to talks on tuesday before m2

--- utils/analysis.py
+++ utils/analysis.py
@@ -3,6 +3,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 from scipy import stats 
+from collections import Counter
 
 def visualize_article_connections_per_category(connections, articles_categories, description):
   graph = nx.DiGraph()
@@ -34,3 +35,29 @@ def t_test_article_metrics(metrics, df_1, df_2):
   for metric in metrics:
     statistic, pvalue = stats.ttest_ind(df_1[metric], df_2[metric], nan_policy="omit", equal_var=False)
     print("\t - {} - t-statistic: {:.3f}, p-value: {:.3f}".format(metric, statistic, pvalue))
+
+def sorted_category_counts(df, category_dict):
+  all_target_broad_categories = [
+    category_dict[target] for target in df["target"] if target in category_dict
+  ]
+  all_target_broad_categories = [item for sublist in all_target_broad_categories for item in sublist]
+  count_cats_finished_target = Counter(all_target_broad_categories)
+  keys_finished = list(count_cats_finished_target.keys())
+  keys_finished.sort()
+  sorted_cats = {i: count_cats_finished_target[i] for i in keys_finished}
+  return sorted_cats
+
+def shortest_path_find(df, articles, shortest_paths):
+  shortest_unfinished = []
+  not_found = 0
+  for i in range(len(df)):
+      source = articles.loc[articles['article'] == df.iloc[i]["path"][0]]
+      target = articles.loc[articles['article'] == df.iloc[i]["target"]]
+      if len(source) != 0 and len(target) != 0:
+          index_source = source.index[0]
+          index_target = target.index[0]
+          shortest_unfinished.append(int(shortest_paths[index_source][index_target]))
+      else:
+          shortest_unfinished.append(None)
+          not_found+=1
+  return shortest_unfinished, not_found

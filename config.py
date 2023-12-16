84843955a6c502383e0945167100764bb369ec04
Juan Garcia Giraldo <juan.garciagiraldo@student.manchester.ac.uk>
2023-12-16 17:45:22 +0100

    Updating graphs

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ PATH_TO_DATA = r"./data" # path to the folder containing the raw data
 PATH_GRAPH_FOLDER = "wikispeedia_paths-and-graph"
 ARTICLE_FOLDER = "plaintext_articles"
 GENERATED_METRICS = "generated_data"
+DATASTORY_PLOTS = "datastory_plots"
